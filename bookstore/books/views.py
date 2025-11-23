7daf4a7638e280c5b013e90ce4ca118ad24a02bc
Raj Patel <raj.patel514@gmail.com>
2025-11-23 05:53:05 -0500

    Update

--- bookstore/books/views.py
+++ bookstore/books/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Book
 
-# Create your views here.
+def homepage(request):
+    books = Book.objects.all()
+    return render(request, 'books/home.html', {'books': books})
+
+def book_detail(request, id):
+    return render(request, 'books/detail.html')
